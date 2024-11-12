82c44426179002cdad77c65300dcc61a220e4f52
Daniel Yacobi <danieljoshua.yacobi@post.runi.ac.il>
2024-11-12 19:53:58 +0200

    Daniel

--- Collatz.java
+++ Collatz.java
@@ -2,6 +2,14 @@
 public class Collatz {
 	public static void main(String args[]) {
 
-		
+		int N = Integer.parseInt(args[0]); //highest seed, via input
+		String mode = args[1];
+		boolean reached1 = true;
+		int i = 1;
+		int numinrow;
+
+		while (i <= N) {
+			
+		}
 	}
 }
